fd2f0f8b4e6a6ae83f9462687653e6c6297f6f86
Paweł Pawiłojć <pawilojc@gmail.com>
2020-03-25 14:13:48 +0100

    start

---
+++ src/main/java/pl/pawilojc/FowChessApplication.java
@@ -0,0 +1,13 @@
+package pl.pawilojc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FowChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FowChessApplication.class, args);
+	}
+
+}
